e5fc45e16ab6fde04485db587ebf7e8ad593dfbe
masud rana <masudranaacb@gmail.com>
2022-04-06 23:41:13 +0600

    conditional randaring added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from 'react';
 const auth = getAuth(app);
 function App() {
-  const [user,setUser]=useState({})
-  const {displayName,email}=user
+  const [user, setUser] = useState({})
+  const { displayName, email } = user
   const provider = new GoogleAuthProvider();
-  const handelSignIn=()=>{
+
+  const handelSignIn = () => {
     signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-      
-      setUser(user)
-      console.log(user)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+
+        setUser(user)
+        console.log(user)
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(error)
+      });
+  }
+
+  const singOutHandel = () => {
+    signOut(auth).then(() => {
+      setUser({})
+      console.log('logout')
     }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      console.log(error)
+      setUser({})
     });
   }
   return (
     <div className="App">
-      <button onClick={handelSignIn}> Googke sign-in</button>
+      {
+        email?<button onClick={singOutHandel}> Long Out</button>:
+      <button onClick={handelSignIn}>sign-in</button>
+      }
       <div>
         <h3>Name:{displayName}</h3>
         <h3>Email:{email}</h3>
